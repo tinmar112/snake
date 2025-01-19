174bbae26b52c1ef31505b5ccf4c372424d5756b
tinmar112 <martin.zadora@etu.minesparis.psl.eu>
2025-01-19 16:12:08 +0100

    Delete snake/packages directory
    Error linked to a wrong git synchro

--- snake/packages/dir.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class Dir(enum.Enum):
-    UP = (0, -1)
-    DOWN = (0, 1)
-    LEFT = (-1, 0)
-    RIGHT = (1, 0)
--- snake/packages/exceptions.py
+++
@@ -1,7 +0,0 @@
-class GameOver(Exception):
-
-    def __init__(self,message: str) -> None:
-        self._message = message
-    
-    def __str__(self):
-        return f"Game over! {self._message}"
